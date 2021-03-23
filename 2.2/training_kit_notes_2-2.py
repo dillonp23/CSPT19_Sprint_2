bc28209bec79f1d377f962cba46f25659a274a8b
Dillon P <52013691+dillonp23@users.noreply.github.com>
2021-03-23 16:23:33 -0500

    Added info about basic properties of linked list data structures

--- 2.2/training_kit_notes_2-2.py
+++ 2.2/training_kit_notes_2-2.py
@@ -3,3 +3,46 @@
 """
 Sprint 2.2 - Linked Lists
 """
+
+
+"""
+Objective 1: Time/space complexity, strengths & weaknesses, and common uses of a linked list
+
+    * Basic properties of a linked list:
+        1. simple, linear data structure
+
+        2. a collection of elements that doesn't require contiguous space in memory
+            - can spread out data stored in memory
+
+        3. made up of nodes holding both:
+            - data
+            - pointer to next node (or next and prev if doubly-linked)
+
+
+    * What types of data can linked lists be used for?
+        - strings, numbers, booleans, even other data structures
+        - the type of data is not necessarily the limitng factor when it comes to linked lists
+
+    
+    * Are elements sorted or unsorted in linked list?
+        - elements can be either or
+        - nothing explicitly forces linked list to be sorted or not
+        - knowing data is stored in a linked list does not tell you whether its sorted
+
+    
+    * Can linked lists contain duplicates?
+        - yes
+        - nothing prevents dupicates from being stored
+            - in contrast to another structure like a Set
+
+        
+    * What are the different types of linked lists?
+        - three different types, but all having same basic structure
+        - node with data and pointer(s) to next (and prev) node(s)
+
+        1. singly linked list (SLL)
+
+        2. doubly linked list (DLL)
+
+        3. circular linked list
+"""
