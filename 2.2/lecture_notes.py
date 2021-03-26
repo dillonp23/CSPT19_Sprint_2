6cba9b07759b9673a00299e29d64d754e57eddb4
Dillon P <52013691+dillonp23@users.noreply.github.com>
2021-03-25 21:47:11 -0500

    Added prompt for exercise 2

--- 2.2/lecture_notes.py
+++ 2.2/lecture_notes.py
@@ -70,3 +70,16 @@ def deleteNode(list, node):
 
 # modifying node in place - don't return anything
 # the given node will be associated with a list, but we will not know the list elements or be given the head
+
+
+
+
+"""
+Exercise 2: "206. Reverse Linked List" (https://leetcode.com/problems/reverse-linked-list/)
+
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+* Examples:
+    Input: head = [1,2,3,4,5]
+    Output: [5,4,3,2,1]
+"""
