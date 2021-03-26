5c3cbaed4ec037e8d4474a478152fa27111edd03
Dillon P <52013691+dillonp23@users.noreply.github.com>
2021-03-25 21:44:56 -0500

    Added solution for exercise 1 to alter a node in place

--- 2.2/lecture_notes.py
+++ 2.2/lecture_notes.py
@@ -44,5 +44,29 @@ def traverseLinkedList(startNode):
         print(curr)
         curr = curr.next
 
- 
+
 traverseLinkedList(a)
+
+
+"""
+Exercise 1: "237. Delete Node in a Linked List" (https://leetcode.com/problems/delete-node-in-a-linked-list/)
+
+Write a function to delete a node in a singly-linked list. You will not be given access to the head of the list, 
+instead you will be given access to the node to be deleted directly.
+
+It is guaranteed that the node to be deleted is not a tail node in the list.
+
+* Example:
+    Input: head = [4,5,1,9], node = 5
+    Output: [4,1,9]
+    Explanation: You are given the second node with value 5, the linked list should become 4 -> 1 -> 9 after calling your function.
+"""
+
+def deleteNode(list, node):
+    next = node.next
+    node.val = next.val
+    node.next = next.next
+
+
+# modifying node in place - don't return anything
+# the given node will be associated with a list, but we will not know the list elements or be given the head
