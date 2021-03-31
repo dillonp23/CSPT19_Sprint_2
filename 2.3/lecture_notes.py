273247725320a127bc0cdab57b9f58c127b5abb4
Dillon P <52013691+dillonp23@users.noreply.github.com>
2021-03-30 20:16:39 -0500

    Added prompt for exercise 1 of lecture

--- 2.3/lecture_notes.py
+++ 2.3/lecture_notes.py
@@ -93,3 +93,39 @@ my_deque.push(5)
 print(my_deque.data)
 print(my_deque.pop())
 print(my_deque.data)
+
+
+
+
+"""
+Exercise 1: "155. MinStack" (https://leetcode.com/problems/min-stack/)
+
+Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+Implement the MinStack class:
+    MinStack() initializes the stack object.
+    void push(val) pushes the element val onto the stack.
+    void pop() removes the element on the top of the stack.
+    int top() gets the top element of the stack.
+    int getMin() retrieves the minimum element in the stack.
+ 
+
+* Example:
+
+    Input:
+        ["MinStack","push","push","push","getMin","pop","top","getMin"]
+        [[],[-2],[0],[-3],[],[],[],[]]
+
+    Output:
+        [null,null,null,null,-3,null,0,-2]
+
+    Explanation:
+        MinStack minStack = new MinStack();
+        minStack.push(-2);
+        minStack.push(0);
+        minStack.push(-3);
+        minStack.getMin(); // return -3
+        minStack.pop();
+        minStack.top();    // return 0
+        minStack.getMin(); // return -2
+"""
