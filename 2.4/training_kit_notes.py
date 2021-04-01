8ba38f6ef7c0c8823ce57db7f48e1052b01c92b5
Dillon P <52013691+dillonp23@users.noreply.github.com>
2021-03-31 20:34:34 -0500

    Added a simple linear search example algorithm

--- 2.4/training_kit_notes.py
+++ 2.4/training_kit_notes.py
@@ -43,4 +43,41 @@ Objective 1: Understanding Logarithms
                 
                 3. perfect binary trees
                     - number of nodes doubles at each level
+
+
+
+Objective 2: Linear Search Algorithm
+
+Imagine that I've chosen a random number from 1 to 20. Then, you must guess the number. One approach would be to start 
+picking at 1 and increment your guess by 1 with each guess. If the computer randomly selected 20, then it would take 
+you 20 guesses to get the correct answer. If the computer guessed 1, then you would have the right answer on your very 
+first guess. On average, you will get the correct answer on the 10th or 11th guess.
+
+If the collection we are searching through is random and unsorted, linear search is the most efficient way to search 
+through it. Once we have a sorted list, then there are more efficient approaches to use.
+
+
+Write a simple program to conduct a linear search on a collection of data.
 """
+
+def linear_search(arr, target):
+
+    # loop through and check if item in input array is the target
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
+        
+    # if target not in array return index out of range
+    return -1
+
+
+example_arr = [12,4,5,25,9,7,3,1,2]
+target_1 = 8
+target_2 = 9
+target_3 = 0
+target_4 = 10
+
+print(linear_search(example_arr, target_1))
+print(linear_search(example_arr, target_2))
+print(linear_search(example_arr, target_3))
+print(linear_search(example_arr, target_4))
