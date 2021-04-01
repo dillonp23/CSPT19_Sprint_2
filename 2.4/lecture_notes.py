d3d83d8fe5c97f7d7954e3c05de36af0d8a58995
Dillon P <52013691+dillonp23@users.noreply.github.com>
2021-03-31 21:03:49 -0500

    Added prompt for exercise 2

--- 2.4/lecture_notes.py
+++ 2.4/lecture_notes.py
@@ -60,3 +60,32 @@ def search(nums, target):
 
 nums = [0,13,22,43,45,56,62,73,80,92,102]
 print(search(nums, 73))
+
+
+
+
+"""
+Exercise 2: "374. Guess Number Higher or Lower" (https://leetcode.com/problems/guess-number-higher-or-lower/)
+
+We are playing the Guess Game. The game is as follows:
+
+    - I pick a number from 1 to n. You have to guess which number I picked.
+
+    - Every time you guess wrong, I will tell you whether the number I picked is higher or lower than your guess.
+
+    - You call a pre-defined API int guess(int num), which returns 3 possible results:
+
+    -1: The number I picked is lower than your guess (i.e. pick < num).
+    1: The number I picked is higher than your guess (i.e. pick > num).
+    0: The number I picked is equal to your guess (i.e. pick == num).
+    Return the number that I picked.
+
+
+* Examples:
+
+    Input: n = 10, pick = 6
+    Output: 6
+
+    Input: n = 1, pick = 1
+    Output: 1
+"""
