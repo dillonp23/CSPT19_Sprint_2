b0027305283459e842a2822ae776f765efb038db
Dillon P <52013691+dillonp23@users.noreply.github.com>
2021-03-31 21:00:41 -0500

    Added solution to exercise 1

--- 2.4/lecture_notes.py
+++ 2.4/lecture_notes.py
@@ -21,7 +21,13 @@ Sprint 2.4 Lecture Notes - Searching & Recursion
         4. repeat
 
 
+* Hint: whenever you see the word "sorted" think binary search
+"""
 
+
+
+
+"""
 * Exercise 1: "704. Binary Search" (https://leetcode.com/problems/binary-search/)
 
 Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search
@@ -33,3 +39,24 @@ target in nums. If target exists, then return its index. Otherwise, return -1.
     Output: 4
     Explanation: 9 exists in nums and its index is 4
 """
+
+def search(nums, target):
+
+    start, end = 0, len(nums) - 1
+
+    while start <= end:
+        mid = (start + end) // 2
+
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            start = mid + 1
+        else:
+            end = mid - 1
+
+
+    return -1
+
+
+nums = [0,13,22,43,45,56,62,73,80,92,102]
+print(search(nums, 73))
