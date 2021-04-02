e9ce799d026666f96053316ff36a9229b4c2c913
Dillon P <52013691+dillonp23@users.noreply.github.com>
2021-04-01 21:32:34 -0500

    Added prompt and answer for task number 3 multiple choice question

--- 2.4/module_project.py
+++ 2.4/module_project.py
@@ -23,4 +23,22 @@ might alert you that logarithims are involved?
     - divide in half
     - binary search
     - double
+
+
+
+* Task 3 (multiple choice):
+
+What type of search algorithm is used in this representation?
+
+    1. {9-7-6-4-3-2-1} --- {6}
+            
+    2. {9-7-6-4-3-2-1} --- {4 < 6}
+              ^
+    3. {9-7-6}-4-3-2-1 --- {7 > 6}
+          ^
+    4. 9-7-{6}-4-3-2-1 --- {6 == 6}
+            ^
+
+* Answer:
+    - binary search
 """
