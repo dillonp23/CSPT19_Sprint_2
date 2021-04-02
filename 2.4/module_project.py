f4d9b435b7af779a13af1d05cff0a34cf60c9d98
Dillon P <52013691+dillonp23@users.noreply.github.com>
2021-04-01 21:38:12 -0500

    Added prompt and solution for task 4 multiple choice question

--- 2.4/module_project.py
+++ 2.4/module_project.py
@@ -14,6 +14,7 @@ Which logarithmic expression is identical to the following exponential expressio
 
 
 
+
 * Task 2 (multiple choice):
 
 When solving an algorithmic coding challenge, which keywords should you look out for that 
@@ -23,6 +24,7 @@ might alert you that logarithims are involved?
     - divide in half
     - binary search
     - double
+
 
 
 
@@ -41,4 +43,32 @@ What type of search algorithm is used in this representation?
 
 * Answer:
     - binary search
+
+
+
+
+* Task 4 (multiple choice):
+
+Given an example of a binary search algorithm, what must be true?
+
+    def binary_search(item_list, item):
+        first = 0
+        last = len(item_list) - 1
+
+        while first <= last:
+            mid = (first + last) // 2
+
+            if item_list[mid] == item:
+                return True
+            else:
+                if item < item_list[mid]:
+                    last = mid - 1
+                else:
+                    first = mid + 1
+        
+        return False
+
+
+* Answer:
+    - item_list must be sorted from smallest to greatest
 """
