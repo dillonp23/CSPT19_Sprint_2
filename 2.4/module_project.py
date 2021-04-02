65f020f03f78f3df5037eebdbdaf22b0c7eeca5e
Dillon P <52013691+dillonp23@users.noreply.github.com>
2021-04-01 21:15:29 -0500

    Added prompt and answer for task 1 multiple choice question

--- 2.4/module_project.py
+++ 2.4/module_project.py
@@ -1,4 +1,14 @@
 
 """
 Sprint 2.4 - Binary Search & Recursion CodeSignal Assignment
+
+
+* Task 1 (multiple choice):
+
+Which logarithmic expression is identical to the following exponential expression?
+
+    2^n = 64
+
+* Answer:
+    log_2(64) = n
 """
