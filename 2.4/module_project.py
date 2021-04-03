79cbebdca920a19740d43b86d1b9b86c50ab25c8
Dillon P <52013691+dillonp23@users.noreply.github.com>
2021-04-02 19:56:46 -0500

    Added first test case for exercise 3 solution. Added detailed notes for future reference

--- 2.4/module_project.py
+++ 2.4/module_project.py
@@ -312,3 +312,33 @@ def csSearchRotatedSortedArray(nums, target):
 
     
     return expected_index - difference
+
+
+
+# Test case 1:
+print("\nExercise 3 - Search Sorted & Rotated Array")
+target = 20
+nums = [7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,1,2,3,4,5,6]
+result = csSearchRotatedSortedArray(nums, target)
+print(result)
+print(nums[result] == target)
+assert nums[result] == target
+
+"""
+[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]
+           ^ 
+pivot point @ 5th index: value = 6
+
+
+[7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,1,2,3,4,5,6]
+              ^                                                                 ^
+    new 5th index: value = 12                                          prev 5th index @ 29th index
+
+diff = nums[0] - 1 ==> 7 - 1 ==> 6
+target = 20
+expected_index = 20 - 1 ==> 19
+new_index = expected_index - diff ==> 19 - 6 ==> 13
+
+return new_index  ==> 13
+i.e. nums[13] = target
+"""
