1304083ee4357400769fb84134a4df1d06235899
Dillon P <52013691+dillonp23@users.noreply.github.com>
2021-04-04 19:53:11 -0500

    Added prompt for exercise 2

--- 2.3/lecture_notes.py
+++ 2.3/lecture_notes.py
@@ -287,3 +287,41 @@ print(myQueue.queue)
 
 myQueue.dequeue()
 print(myQueue.queue)
+
+
+
+
+"""
+Exercise 2: "225. Implement Stack using Queues" (https://leetcode.com/problems/implement-stack-using-queues/)
+
+Implement a last in first out (LIFO) stack using only two queues. The implemented stack should support all the 
+functions of a normal queue (push, top, pop, and empty).
+
+* Implement the MyStack class:
+    - void push(int x) Pushes element x to the top of the stack.
+    - int pop() Removes the element on the top of the stack and returns it.
+    - int top() Returns the element on the top of the stack.
+    - boolean empty() Returns true if the stack is empty, false otherwise.
+
+* Notes:
+    - Use only standard operations of a queue: push to back, peek/pop front, size, and isEmpty operations
+    - Depending on your language, the queue may not be supported natively. You may simulate a queue using a list or 
+        deque (double-ended queue), as long as you use only a queue's standard operations.
+    
+
+* Example:
+    Input:
+        ["MyStack", "push", "push", "top", "pop", "empty"]
+        [[], [1], [2], [], [], []]
+
+    Output:
+        [null, null, null, 2, 2, false]
+
+    Explanation:
+        MyStack myStack = new MyStack();
+        myStack.push(1);
+        myStack.push(2);
+        myStack.top(); // return 2
+        myStack.pop(); // return 2
+        myStack.empty(); // return False
+"""
