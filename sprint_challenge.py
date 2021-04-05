1823cc69dcf5f66deecb99b511d15bb81d8cea0e
Dillon P <52013691+dillonp23@users.noreply.github.com>
2021-04-04 20:58:07 -0500

    Added prompt for exercise 2

--- sprint_challenge.py
+++ sprint_challenge.py
@@ -52,3 +52,42 @@ c.next = d
 print(a)
 b = reverseLinkedList(a)
 print(b)
+
+
+
+
+"""
+Exercise 2: Check Blanagrams (task 4 of 9)
+
+Two words are blanagrams if they are anagrams, but exactly one letter has been substituted for another.
+
+Given two words, check if they are blanagrams of each other.
+
+* Examples:
+    For word1 = "tangram" and word2 = "anagram"
+    checkBlanagrams(word1, word2) = true
+
+    After changing the first letter 't' to 'a' in word1, the words become anagrams.
+
+
+    For word1 = "tangram" and word2 = "pangram"
+    checkBlanagrams(word1, word2) = true
+
+    Since a word is an anagram of itself (a so-called trivial anagram), we are not obliged to rearrange letters. 
+    Only the substitution of a single letter is required for a word to be a blanagram, and here 't' is changed to 'p'.
+
+
+    For word1 = "aba" and word2 = "bab"
+    checkBlanagrams(word1, word2) = true
+
+    You can take the first letter 'a' of word1 and change it to 'b', obtaining the word "bba", which is an anagram 
+    of word2 = "bab". It is also possible to change the first letter 'b' of word2 to 'a' and obtain "aab", which 
+    is an anagram of word1 = "aba".
+
+
+    For word1 = "silent" and word2 = "listen"
+    checkBlanagrams(word1, word2) = false
+
+    These two words are anagrams of each other, but no letter substitution was made (the trivial substitution of a letter 
+    with itself shouldn't be considered), so they are not blanagrams.
+"""
