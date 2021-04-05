a08f153b5566393ef575196e985331690b10c1b8
Dillon P <52013691+dillonp23@users.noreply.github.com>
2021-04-04 20:39:11 -0500

    Added solution and test case for exercise 1

--- sprint_challenge.py
+++ sprint_challenge.py
@@ -16,3 +16,39 @@ Given a singly linked list, reverse and return it.
     For l = [1, 2, 3, 4, 5], the output should be
     reverseLinkedList(l) = [5, 4, 3, 2, 1]
 """
+
+class ListNode(object):
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+    def __repr__(self):
+        return f"{self.value}->{self.next}"
+
+
+def reverseLinkedList(l):
+    curr = l
+    prev = None
+
+    while curr:
+        temp = curr.next
+        curr.next = prev
+        prev = curr
+        curr = temp
+    
+    return prev
+
+
+print("Exercise 1: Reverse a Linked List")
+a = ListNode(1)
+b = ListNode(2)
+c = ListNode(3)
+d = ListNode(4)
+
+a.next = b
+b.next = c
+c.next = d
+
+print(a)
+b = reverseLinkedList(a)
+print(b)
