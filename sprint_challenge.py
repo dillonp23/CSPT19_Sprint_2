023786624228cdf09c2b4764c138374160333f36
Dillon P <52013691+dillonp23@users.noreply.github.com>
2021-04-04 20:10:50 -0500

    Added file for Sprint 2 challenge. Added prompt for exercise 1

---
+++ sprint_challenge.py
@@ -0,0 +1,18 @@
+
+"""
+Sprint Challenge #2 - Data Structures and Algorithms I
+"""
+
+
+"""
+Exercise 1: Reverse a Linked List (Task 1 of 9)
+
+Given a singly linked list, reverse and return it.
+
+* Notes: 
+    - your solution should have O(n) time complexity and O(1) space complexity
+
+* Example:
+    For l = [1, 2, 3, 4, 5], the output should be
+    reverseLinkedList(l) = [5, 4, 3, 2, 1]
+"""
