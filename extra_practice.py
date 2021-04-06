041537d507c41a6c145b7c8cccdeccba091b26db
Dillon P <52013691+dillonp23@users.noreply.github.com>
2021-04-05 22:18:05 -0500

    Implemented binary search function to find pivot index of original array

--- extra_practice.py
+++ extra_practice.py
@@ -34,7 +34,7 @@ Your algorithm's runtime complexity must be in the order of O(log n).
         - array in ascending order so we can use a modified binary search algo
             - needs to be modified as there is a pivot point
         - characteristics of the pivot point:
-            - nums[pivot_index] will be greater than nums[pivot_index + 1] as ascedning order
+            - nums[pivot_index] will be greater than nums[pivot_index+1] as ascedning order
             - only one pivot point
 
     * plan:
@@ -44,7 +44,7 @@ Your algorithm's runtime complexity must be in the order of O(log n).
             - if target between lhs[0] and lhs[pivot_index]
                 - do binary search with start = 0 and end = pivot_index
             else:
-                - start = pivot_index and end = len(array) - 1
+                - start = pivot_index and end = len(array)-1
         - utilize primary function and two helpers
             - 1st helper: modified binary search to find pivot_index
             - 2nd: standard bin search (using pivot_index as start or end) to find target
@@ -55,8 +55,28 @@ def findTargetInPivotedArray(nums, target):
 
 
 # helper 1:
+# looking for pivot -> index where nums[i] > nums[i+1]
+# use recursion to narrow search
 def getPivotIndex(nums, start, end):
-    pass
+    mid = (start + end) // 2
+
+    # base cases
+    if mid > end:
+        return -1
+    if start == end:
+        return start
+
+    # Check if mid or mid-1 is pivot point and return index
+    if nums[start] < nums[mid] and nums[mid] > nums[mid+1]:
+        return mid
+    elif nums[start] < nums[mid] and nums[mid] < nums[mid-1]:
+        return mid-1
+
+    # if no pivot found yet, recursively call
+    if nums[start] > nums[mid] and nums[mid] < nums[end]:
+        return getPivotIndex(nums, start, mid-1)
+    else:
+        return getPivotIndex(nums, mid+1, end)
 
 
 # helper 2:
@@ -68,4 +88,11 @@ def sortedBinarySearch(nums, start, end, target):
 print("Exercise 1: Find Target Index In Pivoted Array") 
 # nums = [4,5,1,2,3], target = 3 ==>> expected: 4
 # nums = [8,9,10,11,12,13,14,15,16,1,2,3,4,5,6,7], target = 2 ==>> expected: 10
-# nums = [60...100, 1...59], target = 12 ==>> expected: 51
+# nums = [60...100, 1...59], target = 12 ==>> expected: 41
+nums = []
+for i in range(80, 101):
+    nums.append(i)
+for i in range(1,80):
+    nums.append(i)
+print(nums)
+print(getPivotIndex(nums, 0, len(nums)-1))
