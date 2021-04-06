e012ca8a1db879e7fc9e85053efd7b1e45cfb84b
Dillon P <52013691+dillonp23@users.noreply.github.com>
2021-04-06 11:01:27 -0500

    Added ListNode class/init and implemented LinkedList initializer

--- extra_practice.py
+++ extra_practice.py
@@ -214,46 +214,38 @@ Implement the MyLinkedList class:
     myLinkedList.get(1)              # return 3
 """
 
+class ListNode:
+    def __init__(self, val=None, next=None):
+        self.val = val
+        self.next = next 
+
+
 class MyLinkedList:
 
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
+        self.head = None
+        self.tail = None
         
 
-    def get(self, index: int) -> int:
-        """
-        Get the value of the index-th node in the linked list. If the index is invalid, return -1.
-        """
+    def get(self, index: int) -> int:        
+        pass
         
 
     def addAtHead(self, val: int) -> None:
-        """
-        Add a node of value val before the first element of the linked list. After the insertion, the new node 
-        will be the first node of the linked list.
-        """
+        pass
         
 
     def addAtTail(self, val: int) -> None:
-        """
-        Append a node of value val to the last element of the linked list.
-        """
+        pass
         
 
     def addAtIndex(self, index: int, val: int) -> None:
-        """
-        Add a node of value val before the index-th node in the linked list. If index equals to the length of 
-        linked list, the node will be appended to the end of linked list. If index is greater than the length, 
-        the node will not be inserted.
-        """
-        
+        pass
+
 
     def deleteAtIndex(self, index: int) -> None:
-        """
-        Delete the index-th node in the linked list, if the index is valid.
-        """
-        
+        pass
+
 
 
 # Your MyLinkedList object will be instantiated and called as such:
